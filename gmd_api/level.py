71e2f366a4f839546434b0a9f719265d81f972c2
veprogames <75524847+veprogames@users.noreply.github.com>
2024-05-04 11:11:20 +0200

    api to modify inner level string

--- gmd_api/level.py
+++ gmd_api/level.py
@@ -1,30 +1,23 @@
 from .level_object import LevelObject
-from .color import get_default_colors, ColorChannel
+from .color import ColorChannel
+from .level_string import InnerLevelString
 from typing import Self
-import gzip
-from base64 import urlsafe_b64encode
 
 class Level:
     def __init__(self, name: str = "") -> None:
         self.name: str = name
-        self.objects: list[LevelObject] = []
-        self.color_channels: list[ColorChannel] = get_default_colors()
+        self.inner_string: InnerLevelString = InnerLevelString()
 
     def add_object(self, obj: LevelObject) -> Self:
-        self.objects.append(obj)
+        self.inner_string.objects.append(obj)
         return self
     
     def add_color_channel(self, channel: ColorChannel) -> Self:
-        self.color_channels.append(channel)
+        self.inner_string.color_channels.append(channel)
         return self
     
-    def get_level_string(self) -> str:
-        colors: str = "".join([channel.to_string() for channel in self.color_channels])
-        prelude: str = f"kS38,{colors},kA13,0,kA15,0,kA16,0,kA14,,kA6,0,kA7,0,kA25,0,kA17,0,kA18,0,kS39,0,kA2,0,kA3,0,kA8,0,kA4,0,kA9,0,kA10,0,kA22,0,kA23,0,kA24,0,kA27,1,kA40,1,kA41,1,kA42,1,kA28,0,kA29,0,kA31,1,kA32,1,kA36,0,kA43,0,kA44,0,kA45,1,kA33,1,kA34,1,kA35,0,kA37,1,kA38,1,kA39,1,kA19,0,kA26,0,kA20,0,kA21,0,kA11,0;"
-        objects: str = "".join([obj.to_string() for obj in self.objects])
-
-        raw_level_string = bytes(f"{prelude}{objects}", encoding="utf-8")
-        return urlsafe_b64encode(gzip.compress(raw_level_string)).decode("utf-8")
+    def inner(self) -> InnerLevelString:
+        return self.inner_string
 
     def to_string(self) -> str:
         string: str = f"""<?xml version="1.0"?>
@@ -32,7 +25,7 @@ class Level:
     <dict>
         <k>kCEK</k><i>4</i>
         <k>k2</k><s>{self.name}</s>
-        <k>k4</k><s>{self.get_level_string()}</s>
+        <k>k4</k><s>{self.inner_string.to_string()}</s>
         <k>k21</k><i>2</i>
         <k>k16</k><i>1</i>
         <k>k50</k><i>40</i>
---
+++ gmd_api/level_string.py
@@ -0,0 +1,40 @@
+from .level_object import LevelObject
+from .color import ColorChannel, get_default_colors
+from .keys.inner_level_string import *
+from typing import Self
+import gzip
+from base64 import urlsafe_b64encode
+
+class InnerLevelString:
+    def __init__(self) -> None:
+        self.objects: list[LevelObject] = []
+        self.color_channels: list[ColorChannel] = get_default_colors()
+        self.properties: dict[str, any] = {}
+    
+    def set(self, key: str, value: any) -> Self:
+        self.properties[key] = value
+        return self
+    
+    def set_platformer(self, to: bool = True) -> Self:
+        return self.set(K_IS_PLATFORMER, to)
+    
+    def set_background_id(self, id: int) -> Self:
+        return self.set(K_BG_ID, id)
+    
+    def set_ground_id(self, id: int) -> Self:
+        return self.set(K_GROUND_ID, id)
+    
+    def set_middleground_id(self, id: int) -> Self:
+        return self.set(K_MG_ID, id)
+
+    def to_raw_string(self) -> str:
+        colors: str = "".join([channel.to_string() for channel in self.color_channels])
+        keys: str = ",".join([f"{k},{v}" for k, v in self.properties.items()])
+        prelude: str = f"kS38,{colors},{keys};"
+        objects: str = "".join([obj.to_string() for obj in self.objects])
+
+        return f"{prelude}{objects}"
+    
+    def to_string(self) -> str:
+        raw_level_string = bytes(self.to_raw_string(), encoding="utf-8")
+        return urlsafe_b64encode(gzip.compress(raw_level_string)).decode("utf-8")
