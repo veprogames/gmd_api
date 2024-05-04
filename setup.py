23d15032be07ce10f8bd3d0f8c2169ae68e5f023
veprogames <75524847+veprogames@users.noreply.github.com>
2024-05-04 10:25:35 +0200

    make it pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gmd_api',
+      version='0.1',
+      description='GDShare-based API to create Geometry Dash Levels',
+      url='https://github.com/veprogames/gmd-api',
+      author='veprogames',
+      author_email='75524847+veprogames@users.noreply.github.com',
+      license='MIT',
+      packages=['gmd_api'],
+      zip_safe=False)
