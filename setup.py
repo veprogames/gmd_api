eb0c47794b038f282fe0d964d773c118fdee03e1
veprogames <75524847+veprogames@users.noreply.github.com>
2024-05-05 12:19:50 +0200

    add some scale trigger keys, update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gmd_api',
-      version='0.1',
+      version='0.1.1',
       description='GDShare-based API to create Geometry Dash Levels',
       url='https://github.com/veprogames/gmd-api',
       author='veprogames',
